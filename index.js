9839cc03243aa080f10d3a6ad2555389be231153
Juan Manuel <j-grafi@hotmail.com>
2024-10-31 17:43:52 +0100

    interation 3

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 class SortedList {
-  constructor() {}
+  constructor(items = []) {
+    this.items = items 
+    this.length = this.items.length;
+    
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;6
+  }
 
-  get(pos) {}
+   get(pos) {
+    if (pos < 0 || pos >= this.items.length) {
+      throw new Error("OutOfBounds");
+    }else{
+  
+      return this.items[pos]
+    }
+  }
 
   max() {}
 
@@ -13,5 +28,6 @@ class SortedList {
 
   avg() {}
 }
+
 
 module.exports = SortedList;
